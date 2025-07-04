bd49bd5fb6333bc5f242b74ef1c2e4fa1e30112f
Jhonod <akmjonod@gmail.com>
2025-07-04 21:39:39 +0700

    update deploy cloud run

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodGet {
+        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
+    fmt.Fprintln(w, "Hello, World!")
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+    http.HandleFunc("/hello", helloHandler)
+
+    fmt.Println("Server started at http://localhost:8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        fmt.Println("Server error:", err)
+    }
 }
